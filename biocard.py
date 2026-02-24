c7c4944edffd83683c5d59ef6f38e097dcb41a7b
ayesha40426 <ayesha40426@gmail.com>
2026-02-24 15:18:03 +0530

    Add files via upload

---
+++ biocard.py
@@ -0,0 +1,12 @@
+student_name="Ayesha Siddiqa"
+student_age=20
+course_name="Generative AI"
+college_name="Proudhadevaraya institute of technology"
+email_id="ayesha40426@gmail.com"
+
+print("===========Student Bio==========")
+print("Name:",student_name)
+print("age:",student_age)
+print("course:",course_name)
+print("college:",college_name)
+print("email:",email_id)
