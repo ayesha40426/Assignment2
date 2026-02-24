a610dc727e1fdcdd42426e0a9c163f5b01068824
ayesha40426 <ayesha40426@gmail.com>
2026-02-24 15:31:46 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+
+try:
+    number1=float(input("enter first number:"))
+    number2=float(input("enter second number:"))
+    
+    print("addition:",number1+number2)
+    print("division:",number1/number2)
+    print("modulus:",number1%number2)
+    print("multiplication:",number1*number2)
+
+    if number1!=0:
+     print("division:",number2/number1)
+    else:
+     print("division not possible")
+
+except ValueError:
+  print("please enter valid numeric value not a negative values nor zeros")
